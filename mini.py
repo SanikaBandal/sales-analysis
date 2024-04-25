5f2a6b0f4fba4dcd5bee3d3bf058c9a474fa0190
SanikaBandal <148457681+SanikaBandal@users.noreply.github.com>
2024-04-25 12:58:20 +0530

    Update mini.py

--- mini.py
+++ mini.py
@@ -94,7 +94,6 @@ with cl1:
 with cl2:
     with st.expander("Region_ViewData"):
         region = filtered_df.groupby(by = "Region", as_index = False)["Sales"].sum()
-        st.write(region.style.background_gradient(cmap="Oranges"))
         csv = region.to_csv(index = False).encode('utf-8')
         st.download_button("Download Data", data = csv, file_name = "Region.csv", mime = "text/csv",
                         help = 'Click here to download the data as a CSV file')
