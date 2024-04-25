eadd8978346d287cbecd3b64197180889b530f4c
SanikaBandal <148457681+SanikaBandal@users.noreply.github.com>
2024-04-25 12:53:02 +0530

    Update mini.py

--- mini.py
+++ mini.py
@@ -16,7 +16,7 @@ if fl is not None:
     st.write(filename)
     df = pd.read_csv(filename, encoding = "ISO-8859-1")
 else:
-    os.chdir(r"C:\\Users\\SANIKA\\Desktop\\dsbda mini")
+    os.chdir(r"Superstore.csv")
     df = pd.read_csv("Superstore.csv", encoding = "ISO-8859-1")
 df["Order Date"] = pd.to_datetime(df["Order Date"], dayfirst=True, errors='coerce')
 col1, col2 = st.columns((2))
